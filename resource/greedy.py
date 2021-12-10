f348395ccd0c876d2b880b20d1dad83e803efc47
ynny <yn937391832@sjtu.edu.cn>
2021-12-10 15:29:29 +0800

    Update greedy.py

--- resource/greedy.py
+++ resource/greedy.py
@@ -75,3 +75,34 @@ def greedy_schedule(rs: ResourceScheduler):
                 cur_host.finish_time = max(cur_host.finish_time, core.finish_time)
 
                 i = i + 1
+                
+                
+def rand_schedule(self):# need imporvement
+        # naive
+#       end_time = np.zeros(100)
+#       for i in range(len(self.jobs)):
+#       end_time[i] = 0
+    for job in self.jobs:
+        hid = random.randint(0, self.numHost - 1)
+        cur_host = self.hosts[hid]
+        #cid = random.randint(0, cur_host.num_core - 1)
+        short_time = 0xfffff
+        cid = 0
+        for i in range(cur_host.num_core):
+            if cur_host.cores[i].finish_time < short_time:
+                short_time = cur_host.cores[i].finish_time
+                cid = i
+        core = cur_host.cores[cid]
+
+        for block in job.blocks:
+            # update start/end
+            block.hostid = hid
+            block.coreid = cid
+            block.start_time = core.finish_time
+            block.end_time = core.finish_time + block.data / job.speed
+            # update core start/end
+            core.add_block(block, block.data / job.speed)
+        # update job finish
+        job.finish_time = core.finish_time
+        # update host finish
+        cur_host.finish_time = max(cur_host.finish_time, core.finish_time)
