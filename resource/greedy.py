de54b40c7ca5876e4cf2cd475752057b02b5aa25
ynny <yn937391832@sjtu.edu.cn>
2021-12-10 16:15:21 +0800

    Update greedy.py

--- resource/greedy.py
+++ resource/greedy.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from resource.run import ResourceScheduler, Job, Host, Core, Block, list2int
 
-def shortest_end_time(rs, job, cores, end_time_coreid):
+def shortest_end_time(rs, job_i, job, cores, end_time_coreid):
     if cores == 0:
         return 0
     max_time = 0
@@ -11,7 +11,7 @@ def shortest_end_time(rs, job, cores, end_time_coreid):
     if len(job.blocks) <= cores:
         for i in range(len(job.blocks)):
             max_time = max(max_time, job.blocks[i].data)
-            end_time_coreid[i][cores] = i
+            end_time_coreid[job_i][i][cores] = i
         ans = max_time / speed
         return ans
     core_time = np.zeros(cores)
@@ -23,7 +23,7 @@ def shortest_end_time(rs, job, cores, end_time_coreid):
                 short_time = core_time[i]
                 cid = i
         core_time[cid] = core_time[cid] + block.data
-        end_time_coreid[block.blockid][cores] = cid
+        end_time_coreid[job_i][block.blockid][cores] = cid
     for i in range(cores):
         max_time = max(max_time, core_time[i])
     ans = max_time / speed
@@ -49,26 +49,27 @@ def greedy_schedule(rs: ResourceScheduler):
 
             for i in range(num_jobs):
                 for j in range(num_core + 1):
-                    end_time[i][j] = rs.shortest_end_time(rs.jobs[i], j, end_time_coreid[i])
+                    end_time[i][j] = shortest_end_time(rs, i, rs.jobs[i], j, end_time_coreid)
 
             i = 0
             for job in rs.jobs:
                 num_use = min(num_core, len(job.blocks))
                 core_use = 0
                 j = 0
+                finish_time_now = 0
                 speed = job.speed * (1 - rs.alpha * (num_use - 1))
                 for block in job.blocks:
-                    # block.hostid = hid
-                    # block.coreid = end_time_coreid[i][j][num_use] + core_use
+                    block.hostid = hid
+                    block.coreid = end_time_coreid[i][j][num_use] + core_use
                     core = cur_host.cores[int(block.coreid)]
-                    # block.start_time = core.finish_time
-                    # block.end_time = core.finish_time + block.data / speed
+                    block.start_time = core.finish_time
+                    block.end_time = core.finish_time + block.data / speed
+                    finish_time_now = max(finish_time_now, block.end_time)
                     core.add_block(block, block.data / speed)
 
                     j = j + 1
-                for k in range(num_core):
-                    core = cur_host.cores[k]
-                    core.finish_time = end_time[i][num_use]
+                for core in cur_host.cores:
+                    core.finish_time = finish_time_now
                 # update job finish
                 job.finish_time = core.finish_time
                 # update host finish
