8bbf99c8b0af28f874578b50fb18ad14d77f3bb5
xun <xun@thinkpad-Mac.local>
2021-12-11 22:04:54 +0800

    quick commit

---
+++ run.task1.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=`pwd`
+
+python resource/run.py --task 1 --case input/task1_case1.txt
---
+++ run.task2.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=`pwd`
+
+python resource/run.py --task 2 --case input/task2_case1.txt
